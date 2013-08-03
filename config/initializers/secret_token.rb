5cbac67d0ef4f800bd83f8a0e874c0aeb196e16a
Hephzibah <hephzibah.1989@gmail.com>
2013-08-03 11:20:20 +0530

    created the basic rails app for Rails Girls

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Restaurant::Application.config.secret_key_base = 'c7386d8600aa937b22284a460d2b7155ddbef47a4f02d75fd7b033bce7fde642f0c32d45d0790d34a95f638d09c10393ea90bd1f87ef7e3c09e12b646e6708d8'
